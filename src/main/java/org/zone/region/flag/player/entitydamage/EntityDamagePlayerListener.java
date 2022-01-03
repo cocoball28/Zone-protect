8949b97d98c0dabca0f5530991558577ade44500
Lukamon24 <86870824+Lukamon24@users.noreply.github.com>
2022-01-03 18:57:53 +0530

    #37 fixed

--- src/main/java/org/zone/region/flag/player/entitydamage/EntityDamagePlayerListener.java
+++ src/main/java/org/zone/region/flag/player/entitydamage/EntityDamagePlayerListener.java
@@ -1,13 +1,10 @@
 package org.zone.region.flag.player.entitydamage;
 
 import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.event.EventContextKey;
 import org.spongepowered.api.event.EventContextKeys;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.cause.entity.damage.DamageType;
 import org.spongepowered.api.event.cause.entity.damage.DamageTypes;
-import org.spongepowered.api.event.cause.entity.damage.source.DamageSource;
-import org.spongepowered.api.event.cause.entity.damage.source.DamageSources;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
 import org.zone.ZonePlugin;
 import org.zone.region.Zone;
