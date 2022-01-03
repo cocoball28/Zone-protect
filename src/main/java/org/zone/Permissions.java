b94b8bd378e8df1e08021408c4b7e3da6bbbaecb
Lukamon24 <86870824+Lukamon24@users.noreply.github.com>
2022-01-03 20:38:45 +0530

    Do if u want mose #86

--- src/main/java/org/zone/Permissions.java
+++ src/main/java/org/zone/Permissions.java
@@ -9,7 +9,25 @@ public enum Permissions {
     REGION_ADMIN_INFO("zone.region.admin.info"),
     BYPASS_INTERACTION_DOOR("zone.bypass.interaction.door"),
     BYPASS_INTERACTION_BLOCK("zone.bypass.interaction.block"),
-    REGION_EDIT_BOUNDS("zone.region.edit.bounds");
+    REGION_EDIT_BOUNDS("zone.region.edit.bounds"),
+    FLAG_SET("zone.region.flag.set"),
+    FLAG_GET("zone.region.flag.get"),
+    SET_GREETINGS("zone.region.flag.greetings.message.set"),
+    REMOVE_GREETINGS("zone.region.flag.greetings.message.remove"),
+    SET_LEAVING_MESSAGE("zone.region.flag.leaving.message.set"),
+    REMOVE_LEAVING_MESSAGE("zone.region.flag.leaving.message.remove"),
+    OVERRIDE_PLAYER_FALL_DAMAGE("zone.override.fall.damage.player"),
+    OVERRIDE_PLAYER_ENTITY_DAMAGE("zone.override.entity.damage.player"),
+    OVERRIDE_MONSTER_PREVENTION("zone.override.monster.prevention"),
+    OVERRIDE_GREETINGS("zone.override.greetings"),
+    OVERRIDE_LEAVING("zone.override.leaving"),
+    OVERRIDE_PLAYER_PREVENTION("zone.override.prevention.player"),
+    BYPASS_PLAYER_PREVENTION("zone.bypass.prevention.player"),
+    OVERRIDE_DOOR_INTERACTION("zone.override.interaction.door"),
+    BYPASS_DOOR_INTERACTION("zone.bypass.interaction.door"),
+    OVERRIDE_ITEMFRAME_INTERACTION("zone.override.interaction.itemframe"),
+    BYPASS_ITEMFRAME_INTERACTION("zone.bypass.interaction.itemframe");
+
 
     private final @NotNull String permission;
 
