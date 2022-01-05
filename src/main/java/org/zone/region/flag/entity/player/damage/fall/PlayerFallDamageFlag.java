54834b25914228e926e92ecf84d4e0c22404522c
Lukamon24 <86870824+Lukamon24@users.noreply.github.com>
2022-01-05 17:29:06 +0530

    #113 done

--- src/main/java/org/zone/region/flag/entity/player/damage/fall/PlayerFallDamageFlag.java
+++ src/main/java/org/zone/region/flag/entity/player/damage/fall/PlayerFallDamageFlag.java
@@ -4,12 +4,18 @@ import org.jetbrains.annotations.NotNull;
 import org.zone.region.flag.Flag;
 import org.zone.region.flag.FlagType;
 import org.zone.region.flag.FlagTypes;
+import org.zone.region.group.key.GroupKey;
+import org.zone.region.group.key.GroupKeys;
 
-public class PlayerFallDamageFlag implements Flag.TaggedFlag {
+public class PlayerFallDamageFlag implements Flag.TaggedFlag, Flag.AffectsPlayer {
 
     @Override
     public @NotNull FlagType<?> getType() {
         return FlagTypes.PLAYER_FALL_DAMAGE_FLAG_TYPE;
     }
 
+    @Override
+    public @NotNull GroupKey getRequiredKey() {
+        return GroupKeys.PLAYER_FALL_DAMAGE;
+    }
 }
